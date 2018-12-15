d29245a6d03b6a6d5f69446b9b57b5c141fca042
abdullah alfehaid <sheble1985@gmail.com>
2018-12-15 17:39:20 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king","king"]
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+}
+else
+{
+    alert("Sorry, try agin");
+
+}
 
 console.log("User flipped " + cardOne);
